81520dfb31b5f179fc8fef7c441d51ee7e98ad19
leopekkas <leopekkas@gmail.com>
2026-05-31 20:53:08 +0300

    Add fourth GPU streams exercise, fix imports in code

--- gpu/exercises/04-streams-basics/main.cpp
+++ gpu/exercises/04-streams-basics/main.cpp
@@ -11,7 +11,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
-#include <hip/hip_runtime.h>
 #include "error_checking.hpp"
 
 // GPU kernel definition
--- gpu/exercises/04-streams-basics/solution/main.cpp
+++ gpu/exercises/04-streams-basics/solution/main.cpp
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
-#include <hip/hip_runtime.h>
 #include "error_checking.hpp"
 
 // GPU kernel definition

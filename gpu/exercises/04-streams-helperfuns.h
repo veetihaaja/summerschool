c77c38f01aacfcfcbbfe84863454fa707215a227
Juhana Lankinen <juhana.lankinen@csc.fi>
2026-06-01 11:40:57 +0300

    Add license information

--- gpu/exercises/04-streams-helperfuns.h
+++ gpu/exercises/04-streams-helperfuns.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 CSC - IT Center for Science Ltd. <www.csc.fi>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 __device__ float recip_factpow(float x, int n) {
   float retval(1);
   for (int k = 1; k<= n; ++k) {
